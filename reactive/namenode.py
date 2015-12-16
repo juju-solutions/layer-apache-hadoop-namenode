c43f4f3c928ab915db958236a33e9918cda2685a
Cory Johns <johnsca@gmail.com>
2015-12-16 16:37:23 -0500

    Fixed not sending host nor including namenode in hosts_map

--- reactive/namenode.py
+++ reactive/namenode.py
@@ -12,6 +12,9 @@ from charmhelpers.core import hookenv, unitdata
 @when('hadoop.installed')
 @when_not('namenode.started')
 def configure_namenode():
+    local_hostname = hookenv.local_unit().replace('/', '-')
+    private_address = hookenv.unit_get('private-address')
+    ip_addr = utils.resolve_private_address(private_address)
     hadoop = get_hadoop_base()
     hdfs = HDFS(hadoop)
     hdfs.configure_namenode()
@@ -19,6 +22,7 @@ def configure_namenode():
     hdfs.start_namenode()
     hdfs.create_hdfs_dirs()
     hadoop.open_ports('namenode')
+    utils.update_kv_hosts({ip_addr: local_hostname})
     set_state('namenode.started')
 
 
@@ -31,6 +35,7 @@ def blocked():
 @when('namenode.started', 'datanode.related')
 def send_info(datanode):
     hadoop = get_hadoop_base()
+    local_hostname = hookenv.local_unit().replace('/', '-')
     hdfs_port = hadoop.dist_config.port('namenode')
     webhdfs_port = hadoop.dist_config.port('nn_webapp_http')
 
@@ -38,6 +43,7 @@ def send_info(datanode):
     utils.manage_etc_hosts()
 
     datanode.send_spec(hadoop.spec())
+    datanode.send_host(local_hostname)
     datanode.send_ports(hdfs_port, webhdfs_port)
     datanode.send_ssh_key(utils.get_ssh_key('ubuntu'))
     datanode.send_hosts_map(utils.get_kv_hosts())
@@ -45,7 +51,7 @@ def send_info(datanode):
 
 @when('namenode.started', 'datanode.related')
 @when_not('datanode.registered')
-def waiting(datanode):
+def waiting(datanode):  # pylint: disable=unused-argument
     hookenv.status_set('waiting', 'Waiting for DataNodes')
 
 
