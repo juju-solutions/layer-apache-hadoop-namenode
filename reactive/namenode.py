66ea70166288dbf7df03e75ae7feec1fbf657853
Andrew McLeod <admcleod@gmail.com>
2016-01-29 17:30:27 +0100

    ha hadoop changes

--- reactive/namenode.py
+++ reactive/namenode.py
@@ -7,6 +7,7 @@ from charms.layer.hadoop_base import get_hadoop_base
 from jujubigdata.handlers import HDFS
 from jujubigdata import utils
 from charmhelpers.core import hookenv, unitdata
+from charmhelpers.contrib.hahelpers.cluster import peer_units
 
 
 @when('hadoop.installed')
@@ -17,14 +18,26 @@ def configure_namenode():
     ip_addr = utils.resolve_private_address(private_address)
     hadoop = get_hadoop_base()
     hdfs = HDFS(hadoop)
-    hdfs.configure_namenode()
+    hdfs.configure_namenode(peer_units())
     hdfs.format_namenode()
     hdfs.start_namenode()
     hdfs.create_hdfs_dirs()
     hadoop.open_ports('namenode')
     utils.update_kv_hosts({ip_addr: local_hostname})
     set_state('namenode.started')
+    unitdata.kv().set('hdfscluster.size', '1')
+    unitdata.kv().set('hdfscluster.state', 'standalone')
 
+
+#@when('hdfscluster.ready')
+#def ha_namenodes(datanodes):
+#    global namenodes
+#    namenodes = [hookenv.local_unit().replace('/', '-'), hookenv.local_unit().replace('/', '-')]
+
+#@when_not('hdfscluster.ready')
+#def nonha_namenodes():
+#    global namenodes
+#    namenodes = [hookenv.local_unit().replace('/', '-')]
 
 @when('namenode.started')
 @when_not('datanode.joined')
@@ -32,11 +45,48 @@ def blocked():
     hookenv.status_set('blocked', 'Waiting for relation to DataNodes')
 
 
-@when('namenode.started', 'datanode.joined')
+@when('hadoop.installed', 'hdfscluster.increased')
+def hdfscluster_increased(hdfscluster):
+    hadoop = get_hadoop_base()
+    hdfs = HDFS(hadoop)
+    nodes = hdfscluster.get_nodes()
+    cluster_size = len(nodes) + 1
+    if cluster_size > 1:
+        hdfs.init_sharededits()
+        unitdata.kv().set('hdfscluster.state', 'enabled')
+    else:
+        unitdata.kv().set('hdfscluster.state', 'standalone')
+    unitdata.kv().set('hdfscluster.size', cluster_size)
+    local_hostname = hookenv.local_unit().replace('/', '-')
+    hookenv.log("CLUSTERLOG: hdfs cluster size is now: " + str(cluster_size))
+    hookenv.log("CLUSTERLOG: hdfs cluster nodes are: " + str(nodes) + str(local_hostname))
+    hookenv.log("CLUSTERLOG: unitdata cluster state: " + str(unitdata.kv().get('hdfscluster.state')))
+    hookenv.log("CLUSTERLOG: unitdata cluster size: " + str(unitdata.kv().get('hdfscluster.size')))
+
+
+@when('hdfscluster.decreased')
+def hdfscluster_decreased(hdfscluster):
+    nodes = hdfscluster.get_nodes()
+    cluster_size = len(nodes) + 1
+    if cluster_size > 1:
+        unitdata.kv().set('hdfscluster.state', 'enabled')
+    else:
+        unitdata.kv().set('hdfscluster.state', 'standalone')
+    unitdata.kv().set('hdfscluster.size', cluster_size)
+    local_hostname = hookenv.local_unit().replace('/', '-')
+    hookenv.log("CLUSTERLOG: hdfs cluster size is now: " + str(cluster_size))
+    hookenv.log("CLUSTERLOG: hdfs cluster nodes are: " + str(nodes) + str(local_hostname))
+    hookenv.log("CLUSTERLOG: unitdata cluster state: " + str(unitdata.kv().get('hdfscluster.state')))
+    hookenv.log("CLUSTERLOG: unitdata cluster size: " + str(unitdata.kv().get('hdfscluster.size')))
+
+
+@when('namenode.started', 'datanode.related')
 def send_info(datanode):
     hadoop = get_hadoop_base()
     hdfs = HDFS(hadoop)
     local_hostname = hookenv.local_unit().replace('/', '-')
+    hookenv.log("Peer units are: " + str(peer_units()))
+    namenodes = [local_hostname] + peer_units()
     hdfs_port = hadoop.dist_config.port('namenode')
     webhdfs_port = hadoop.dist_config.port('nn_webapp_http')
 
@@ -45,7 +95,7 @@ def send_info(datanode):
     utils.manage_etc_hosts()
 
     datanode.send_spec(hadoop.spec())
-    datanode.send_namenodes([local_hostname])
+    datanode.send_namenodes(namenodes)
     datanode.send_ports(hdfs_port, webhdfs_port)
     datanode.send_ssh_key(utils.get_ssh_key('hdfs'))
     datanode.send_hosts_map(utils.get_kv_hosts())
@@ -96,6 +146,7 @@ def unregister_datanode(datanode):
     slaves_remaining = list(set(slaves) - set(slaves_leaving))
     unitdata.kv().set('namenode.slaves', slaves_remaining)
     hdfs.register_slaves(slaves_remaining)
+    hdfs.configure_qjm(slaves_remaining)
 
     utils.remove_kv_hosts(slaves_leaving)
     utils.manage_etc_hosts()
