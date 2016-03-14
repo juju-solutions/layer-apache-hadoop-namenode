db92525a8e669e9b6091750b3d33b4123884cd2f
Andrew McLeod <admcleod@gmail.com>
2016-03-14 12:57:53 +0100

    namenode ssh key sharing

--- reactive/namenode.py
+++ reactive/namenode.py
@@ -115,7 +115,6 @@ def send_info(datanode):
 
 @when('namenode-cluster.joined', 'datanode.journalnode.joined', 'zookeeper.ready')
 def configure_ha(cluster, datanode, zookeeper, *args):
-    cluster.send_ssh_key(utils.get_ssh_key('hdfs'))
     cluster_nodes = cluster.nodes()
     cluster.send_ssh_key(utils.get_ssh_key('hdfs'))
     zookeeper_nodes = zookeeper.zookeepers()
@@ -125,9 +124,13 @@ def configure_ha(cluster, datanode, zookeeper, *args):
     hdfs = HDFS(hadoop)
     local_hostname = hookenv.local_unit().replace('/', '-')
     ha_node_state = utils.ha_node_state(local_hostname)
-    if data_changed('cluster.joined', cluster_nodes):
-        utils.update_kv_hosts(cluster.hosts_map())
-        utils.manage_etc_hosts()
+    if data_changed('namenode.pre.ha', cluster_nodes):
+        if hookenv.is_leader():
+            cluster.send_ssh_key_active(utils.get_ssh_key('hdfs'))
+            utils.install_ssh_key('hdfs', cluster.ssh_key_standby())
+        elif not hookenv.is_leader():
+            cluster.send_ssh_key_standby(utils.get_ssh_key('hdfs'))
+            utils.install_ssh_key('hdfs', cluster.ssh_key_active())
     if ha_node_state:
         if not 'ctive' in ha_node_state or not 'andby' in ha_node_state:
             ha_node_state = 'undefined'
@@ -146,7 +149,6 @@ def configure_ha(cluster, datanode, zookeeper, *args):
         #if ha_node_state == 'active':
         if hookenv.is_leader():
             if not is_state('namenode.shared-edits.init'): # and if not namenode.standby.bootstrapped?
-                utils.install_ssh_key('hdfs', cluster.ssh_key())
                 utils.update_kv_hosts(cluster.hosts_map())
                 utils.manage_etc_hosts()
                 hdfs.stop_namenode()
@@ -163,7 +165,6 @@ def configure_ha(cluster, datanode, zookeeper, *args):
         # elif ha_node_state == 'standby':
         elif not hookenv.is_leader():
             if not is_state('namenode.standby.bootstrapped') and cluster.are_jns_init():
-                utils.install_ssh_key('hdfs', cluster.ssh_key())
                 utils.update_kv_hosts(cluster.hosts_map())
                 utils.manage_etc_hosts()
                 hdfs.stop_namenode()
