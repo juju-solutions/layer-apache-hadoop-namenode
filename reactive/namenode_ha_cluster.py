70217df876131c7940408dffd1a5c9551f748c83
Cory Johns <johnsca@gmail.com>
2016-04-15 18:40:49 -0400

    More refactors based on testing and feedback from refactor review

--- reactive/namenode_ha_cluster.py
+++ reactive/namenode_ha_cluster.py
@@ -1,4 +1,3 @@
-from operator import itemgetter
 from charms.reactive import when
 from charms.reactive import when_not
 from charms.reactive import is_state
@@ -21,43 +20,6 @@ def manage_cluster_hosts(cluster):
     utils.manage_etc_hosts()
 
 
-@when('namenode-cluster.joined')
-@when_not('leadership.is_leader')
-def check_cluster_nodes(cluster):
-    hadoop = get_hadoop_base()
-    hdfs_port = hadoop.dist_config.port('namenode')
-
-    manage_cluster_hosts(cluster)  # ensure /etc/hosts is up-to-date
-
-    chosen_nodes = get_cluster_nodes()
-    extra_nodes = sorted(set(cluster.nodes()) - set(chosen_nodes))
-
-    if is_state('namenode.ha.initialized'):
-        # filter down to only responding nodes, preferring
-        # nodes we've already chosen over new nodes
-        viable_nodes = [node for node in chosen_nodes + extra_nodes
-                        if utils.check_connect(node, hdfs_port)]
-    else:
-        # if HA is not yet initialized, then the other node won't
-        # yet be running, so just assume all nodes are viable
-        viable_nodes = chosen_nodes + extra_nodes
-
-    cluster_viable = len(viable_nodes) >= 2
-    hookenv.log('Viable nodes: {}'.format(viable_nodes))
-    toggle_state('namenode.clustered', cluster_viable)
-    return viable_nodes
-
-
-@when('namenode-cluster.joined')
-@when('leadership.is_leader')
-def choose_cluster_nodes(cluster):
-    viable_nodes = check_cluster_nodes(cluster)
-    cluster_viable = len(viable_nodes) >= 2
-    if cluster_viable:
-        # only update chosen nodes if we (again) have enough viable nodes
-        set_cluster_nodes(viable_nodes[:2])
-
-
 @when('datanode.journalnode.joined')
 def check_journalnode_quorum(datanode):
     nodes = datanode.nodes()
@@ -65,23 +27,70 @@ def check_journalnode_quorum(datanode):
     toggle_state('journalnode.quorum', len(nodes) >= quorum_size)
 
 
-@when_not('datanode.journalnode.joined')
-def remove_journalnode_quorum():
-    remove_state('journalnode.quorum')
+@when('namenode-cluster.joined', 'journalnode.quorum')
+@when('leadership.is_leader')
+def enable_ha(cluster):
+    """
+    Once we have two viable NameNodes and a quorum of JournalNodes,
+    inform all cluster units that we are HA.
+
+    Note that this flag is never removed (once HA, always HA) because we
+    could temporarily lose JN quorum or viable NNs during fail-over or
+    restart, and we don't want to arbitrarily revert to non-HA in that case.
+    """
+    leadership.leader_set({'namenode-ha': 'true'})
 
 
-@when('namenode.clustered', 'journalnode.quorum')
-def enable_ha():
-    set_state('namenode.ha')  # once HA, always HA
+@when('leadership.set.ha-initialized')
+@when('namenode-cluster.joined')
+@when_not('leadership.is_leader')
+def check_cluster_health(cluster):
+    cluster_roles = set(utils.ha_node_state(node)
+                        for node in get_cluster_nodes())
+    toggle_state('namenode.cluster.healthy',
+                 cluster_roles == {'active', 'standby'})
 
 
-@when('namenode.ha', 'datanode.joined')
+@when('leadership.set.cluster-activated')
+@when('namenode-cluster.joined')
+@when('leadership.is_leader')
+def check_cluster_nodes(cluster):
+    """
+    Check to see if any of the chosen cluster nodes have gone away and
+    been replaced by viable replacements.
+
+    Note that we only remove a chosen node if it is no longer part of
+    the peer relation *and* has been replaced by a working node.  This
+    ensures that reboots and intermittent node loses don't cause
+    superfluous updates.
+    """
+    local_hostname = hookenv.local_unit().replace('/', '-')
+    hadoop = get_hadoop_base()
+    hdfs_port = hadoop.dist_config.port('namenode')
+
+    manage_cluster_hosts(cluster)  # ensure /etc/hosts is up-to-date
+
+    chosen_nodes = set(get_cluster_nodes())
+    current_nodes = set([local_hostname] + cluster.nodes())
+    remaining_nodes = chosen_nodes & current_nodes
+    added_nodes = current_nodes - chosen_nodes
+
+    if len(remaining_nodes) < 2 and added_nodes:
+        # filter down to only responding nodes
+        viable_nodes = [node for node in added_nodes
+                        if utils.check_connect(node, hdfs_port)]
+        if viable_nodes:
+            set_cluster_nodes(sorted(remaining_nodes) + sorted(viable_nodes))
+
+    check_cluster_health(cluster)  # update health check
+
+
+@when('leadership.set.ha-initialized', 'datanode.joined')
 def update_ha_config(datanode):
     cluster_nodes = get_cluster_nodes()
     jn_nodes = sorted(datanode.nodes())
     jn_port = datanode.jn_port()
     started = is_state('namenode.started')
-    ha_inited = is_state('namenode.ha.initialized')
     new_cluster_config = data_changed('namenode.cluster-nodes', cluster_nodes)
     new_jn_config = data_changed('namenode.jn.config', (jn_nodes, jn_port))
 
@@ -90,86 +99,73 @@ def update_ha_config(datanode):
     hdfs.configure_namenode(cluster_nodes)
     hdfs.register_journalnodes(jn_nodes, jn_port)
 
-    if started and ha_inited and new_cluster_config:
+    if started and new_cluster_config:
         hdfs.restart_namenode()
-    elif started and ha_inited and new_jn_config:
-        hdfs.reload_slaves()
+    elif started and new_jn_config:
+        hdfs.reload_slaves()  # is this actually necessary?
 
 
-@when('namenode.ha', 'datanode.joined')
+@when('leadership.set.namenode-ha')
+@when('namenode-cluster.joined')
+@when('datanode.journalnode.joined')
 @when_not('leadership.set.ha-initialized')
 @when('leadership.is_leader')
-def init_ha_active(datanode):
+def init_ha_active(datanode, cluster):
+    """
+    Do initial HA setup on the leader.
+    """
     local_hostname = hookenv.local_unit().replace('/', '-')
-    update_ha_config(datanode)  # ensure the config is written
     hadoop = get_hadoop_base()
     hdfs = HDFS(hadoop)
     hdfs.stop_namenode()
     remove_state('namenode.started')
+    # initial cluster is us (active) plus a standby
+    set_cluster_nodes([local_hostname, cluster.nodes()[0]])
     update_ha_config(datanode)
     hdfs.init_sharededits()
     hdfs.start_namenode()
-    hdfs.transition_to_active(local_hostname)
     leadership.leader_set({'ha-initialized': 'true'})
-    set_state('namenode.ha.initialized')
     set_state('namenode.started')
 
 
-@when('namenode.ha', 'datanode.joined')
-@when('leadership.set.ha-initialized')
-@when_not('leadership.is_leader')
-@when_not('namenode.ha.initialized')
-def init_ha_standby(datanode):
+@when('namenode-cluster.joined')
+@when('datanode.journalnode.joined')
+@when('leadership.set.ha-initialized')  # wait for leader to init HA
+@when_not('namenode.started')
+def init_ha_standby(datanode, cluster):
+    """
+    Once initial HA setup is done, any new NameNode is started as standby.
+    """
     update_ha_config(datanode)  # ensure the config is written
     hadoop = get_hadoop_base()
     hdfs = HDFS(hadoop)
-    hdfs.stop_namenode()
-    remove_state('namenode.started')
     update_ha_config(datanode)
     hdfs.bootstrap_standby()
     hdfs.start_namenode()
-    set_state('namenode.ha.initialized')
+    cluster.standby_ready()
     set_state('namenode.started')
 
 
-@when('zookeeper.ready')
-@when('namenode.ha.initialized')
-def update_zk_config(zookeeper):
-    hadoop = get_hadoop_base()
-    hdfs = HDFS(hadoop)
-    zk_nodes = sorted(zookeeper.zookeepers(), key=itemgetter('host'))
-    zk_started = is_state('namenode.zk.started')
-    hdfs.configure_zookeeper(zk_nodes)
-    if zk_started and data_changed('namenode.zk', zk_nodes):
-        hdfs.restart_zookeeper()
-
-
-@when('namenode.ha.initialized')
-@when('zookeeper.ready')
-@when_not('leadership.set.zk-formatted')
-@when('leadership.is_leader')
-def format_zookeeper(zookeeper):
-    update_zk_config(zookeeper)  # ensure config is up to date
-    hadoop = get_hadoop_base()
-    hdfs = HDFS(hadoop)
-    hdfs.format_zookeeper()
-    leadership.leader_set({'zk-formatted': 'true'})
-
-
-@when('zookeeper.ready')
-@when('leadership.set.zk-formatted')
-@when_not('namenode.zk.started')
-def start_zookeeper(zookeeper):
-    hadoop = get_hadoop_base()
-    hdfs = HDFS(hadoop)
-    hdfs.start_zookeeper()
-    set_state('namenode.zk.started')
-
-
-@when_not('zookeeper.ready')
-@when('namenode.zk.started')
-def stop_zookeeper():
-    hadoop = get_hadoop_base()
-    hdfs = HDFS(hadoop)
-    hdfs.stop_zookeeper()
-    remove_state('namenode.zk.started')
+# FIXME: The transition to active fails; uncertain if it would fail if done
+#        manually by the admin as well, or if it's just a timing issue; needs
+#        more testing.  It would be nice, though, if the transition to HA would
+#        end up with an active node without manual intervention, even without
+#        Zookeeper in the mix.  But it's not the end of the world if not.
+#
+# @when('leadership.set.ha-initialized')
+# @when('namenode-cluster.standby.ready')
+# @when_not('leadership.set.cluster-activated')
+# @when('leadership.is_leader')
+# def activate_cluster(cluster):
+#     """
+#     Once both the active and standby NameNodes have been started, we
+#     can transition the leader to active.  This must only be done once.
+#     """
+#     chosen_standby = get_cluster_nodes()[1]
+#     if chosen_standby not in cluster.nodes():
+#         return
+#     local_hostname = hookenv.local_unit().replace('/', '-')
+#     hadoop = get_hadoop_base()
+#     hdfs = HDFS(hadoop)
+#     hdfs.transition_to_active(local_hostname)
+#     leadership.leader_set({'cluster-activated': 'true'})
--- reactive/namenode_status.py
+++ reactive/namenode_status.py
@@ -4,6 +4,7 @@ from charms.reactive import when_any
 from charms.reactive import is_state
 from charmhelpers.core import hookenv
 from jujubigdata import utils
+from charms.layer.apache_hadoop_namenode import get_cluster_nodes
 
 
 @when_not('datanode.joined')
@@ -14,32 +15,43 @@ def report_blocked():
 @when('datanode.joined')
 @when_any(
     'namenode.started',
-    'namenode.ha',
+    'leadership.set.namenode-ha',
     'namenode-cluster.joined',
-    'namenode.clustered',
+    'namenode.cluster.healthy',
     'journalnode.quorum',
     'zookeeper.ready',
 )
 def report_status(datanode):
     num_slaves = len(datanode.nodes())
     local_hostname = hookenv.local_unit().replace('/', '-')
+    chosen_nodes = get_cluster_nodes()
+    chosen = local_hostname in chosen_nodes
     started = is_state('namenode.started')
-    ha = is_state('namenode.ha')
-    ha_init = is_state('namenode.ha.initialized')
+    ha = is_state('leadership.set.namenode-ha')
     pending_ha = not ha and is_state('namenode-cluster.joined')
-    pending_role = 'active' if started else 'standby'
     standalone = started and not pending_ha and not ha
-    clustered = is_state('namenode.clustered')
+    clustered = is_state('namenode.cluster.healthy')
     quorum = is_state('journalnode.quorum')
     failover = 'automatic' if is_state('zookeeper.ready') else 'manual'
     degraded = ha and not all([clustered, quorum])
-    ha_role = utils.ha_node_state(local_hostname) if ha_init else 'unknown'
+    if started:
+        pending_role = 'active'
+    elif chosen:
+        pending_role = 'standby'
+    else:
+        pending_role = 'standby or extra'
+    if chosen:
+        ha_role = utils.ha_node_state(local_hostname) or 'down'
+    else:
+        ha_role = 'extra'
 
     if standalone:
         extra = 'standalone'
     elif ha and not degraded:
         extra = '{}, with {} fail-over'.format(ha_role, failover)
     elif (ha and degraded) or pending_ha:
+        if pending_ha:
+            clustered = is_state('namenode-cluster.joined')
         condition = 'pending' if pending_ha else 'degraded'
         role = pending_role if pending_ha else ha_role
         missing = ' and '.join(filter(None, [
